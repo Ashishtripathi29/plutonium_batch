f16fdf48f16c66a31dc1c45fc6a1aae6c6f29a9f
Ashishtripathi29 <ashishtripathipara@gmail.com>
2022-08-28 07:32:20 +0530

    ashish

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -19,8 +19,17 @@ const mid4= function ( req, res, next) {
     console.log("Hi I am a middleware named Mid4")
     next()
 }
+// it my own middleware in below
+
+const appUser=function(req,res,next){
+    const header=req.headers.isfreeappuser;// true or false
+    if(!header) return res.send({msg:"header is importent"})
+    else{ next()}
+}
 
 module.exports.mid1= mid1
 module.exports.mid2= mid2
 module.exports.mid3= mid3
 module.exports.mid4= mid4
+
+module.exports.appUser=appUser
